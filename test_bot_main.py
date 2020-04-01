f95514854e87863cae33dac36ea24f1793275379
karennik98 <karen.nikoghosyan.98@gmail.com>
2020-04-01 19:01:25 +0400

    Creted json file that contein all countris data

--- test_bot_main.py
+++ test_bot_main.py
@@ -2,14 +2,14 @@ import COVID19Py
 import telebot
 
 covid19 = COVID19Py.COVID19()
-location = covid19.getLocationByCountryCode('US')
-print(location)
-date = location[0]['last_updated'].split("T")
-print(date)
+#location = covid19.getLocationByCountryCode('US')
+#print(location)
+#date = location[0]['last_updated'].split("T")
+#print(date)
     
-# locations = covid19.getLocations()
+locations = covid19.getLocations()
 
-# print(locations)
+print(locations)
 # bot = telebot.TeleBot('1016041250:AAHbCS_TlmqG2baVRkc-vnuLwr7H8OrrSUc')
 
 # @bot.message_handler(commands=['start'])
